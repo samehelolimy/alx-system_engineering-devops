da94c7bfbda9819b47c4da1859d86a02d9c6810a
sameh.elolimy <sameh.elolimy@gmail.com>
2024-03-28 02:29:55 -0700

    all

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# regular expression  matches a given pattern
+puts ARGV[0].scan(/hbt*n/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# An expression that  matches only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
