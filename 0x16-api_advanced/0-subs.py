17a0e9eddb0d2c018f5b3077494a4e5009eb9219
sameh <sameh.elolimy@gmail.com>
2024-05-09 17:44:20 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""task module"""
-def number_of_subscribers(subreddit):
-    """ returns the number of subscribers"""
-    import requests
+""" returns the number of total subscribers"""
+import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-    return sub_info.json().get("data").get("subscribers")
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
