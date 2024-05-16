360276aee80d2df4b4daa8062f5b248cae2dd95a
sameh <sameh.elolimy@gmail.com>
2024-05-16 02:37:53 -0700

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path => '/usr/local/bin/:/bin/'
 }
 
 # Increase soft file limit for Holberton user.
